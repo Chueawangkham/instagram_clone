4d593236d3b2b1528c9fc1271e20afaa365ef162
Chueawangkham <thibess091@gmail.com>
2022-07-08 07:12:41 +0100

    save on macos

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
