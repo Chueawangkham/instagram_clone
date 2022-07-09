2103587e51c5cb7933b23e66224f836d0b36707c
Chueawangkham <thibess091@gmail.com>
2022-07-09 19:27:03 +0700

    inital firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
@@ -7,7 +8,20 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyB5gbe7ExXHx64FnTAfFJFG5v8fIczTU54',
+        appId: '1:890165298329:web:b21b8fdb0ef9e0e5412408',
+        messagingSenderId: '890165298329',
+        projectId: 'instagram-tut-11f0c',
+        storageBucket: 'instagram-tut-11f0c.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
